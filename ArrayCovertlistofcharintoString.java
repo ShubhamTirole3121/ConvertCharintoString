ffdc0b78de7caf1e9b4360595ae19869f138da87
ShubhamTirole3121 <84268908+ShubhamTirole3121@users.noreply.github.com>
2021-05-16 15:48:18 +0530

    Add files via upload

---
+++ ArrayCovertlistofcharintoString.java
@@ -0,0 +1,20 @@
+package Schoollevel;
+
+public class ArrayCovertlistofcharintoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char[] ch = new char[] {'a' ,'s','d','r'};
+
+		
+		String str = new String();
+		
+		for(int i=0; i<ch.length; i++) {
+			 str = str + ch[i];
+		}
+		
+		System.out.print(str);
+	}
+
+}
